dab8dca42e9d5c2466db170aa17b33b289fbab69
linknmike <mikeheidal@gmail.com>
2022-12-03 18:14:38 -0800

    Added boilerplate/empty files for remaining days

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,60 @@
 mod read_input;
 mod day_01;
 mod day_02;
+mod day_03;
+mod day_04;
+mod day_05;
+mod day_06;
+mod day_07;
+mod day_08;
+mod day_09;
+mod day_10;
+mod day_11;
+mod day_12;
+mod day_13;
+mod day_14;
+mod day_15;
+mod day_16;
+mod day_17;
+mod day_18;
+mod day_19;
+mod day_20;
+mod day_21;
+mod day_22;
+mod day_23;
+mod day_24;
+mod day_25;
 
 fn main() {
-    day_01::solve();
-    day_02::solve();
+    let should_run_all = true;
+    if should_run_all {
+        day_01::solve();
+        day_02::solve();
+        day_03::solve();
+        day_04::solve();
+        day_05::solve();
+        day_06::solve();
+        day_07::solve();
+        day_08::solve();
+        day_09::solve();
+        day_10::solve();
+        day_11::solve();
+        day_12::solve();
+        day_13::solve();
+        day_14::solve();
+        day_15::solve();
+        day_16::solve();
+        day_17::solve();
+        day_18::solve();
+        day_19::solve();
+        day_20::solve();
+        day_21::solve();
+        day_22::solve();
+        day_23::solve();
+        day_24::solve();
+        day_25::solve();
+    } else {
+        day_03::solve();
+    }
+
 }
