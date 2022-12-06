8fb66f642bac0a53f49117fd831d0f2765b86288
linknmike <mikeheidal@gmail.com>
2022-12-06 13:48:28 -0800

    Refactored to use string slices

--- src/day_06.rs
+++ src/day_06.rs
@@ -3,15 +3,10 @@ use crate::read_input;
 
 
 fn find_index_of_unique_character_set(threshold: usize) -> usize {
-    let mut buffer: Vec<char> = vec!['a'; threshold];
-    let mut buffer_index: usize = 0;
     let input = read_input::read("inputs/06.txt");
-    for (input_index, ch) in input.chars().enumerate() {
-        if &input_index < &threshold || HashSet::<&char>::from_iter(&buffer[..]).len() < threshold {
-            buffer[buffer_index] = ch;
-            buffer_index = (&buffer_index + 1) % &threshold
-        } else {
-            return input_index;
+    for i in threshold..input.len() {
+        if HashSet::<char>::from_iter(input[i-&threshold..i].chars()).len() < threshold {
+            return i
         }
     }
     panic!("Did not find marker in data");
