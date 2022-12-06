e1732e8e0b36c83643c2519f65490390c956bcc9
linknmike <mikeheidal@gmail.com>
2022-12-05 21:49:31 -0800

    Day 6

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,12 +1,28 @@
+use std::collections::HashSet;
 use crate::read_input;
 
+
+fn find_index_of_unique_character_set(threshold: usize) -> usize {
+    let mut buffer: Vec<char> = vec!['a'; threshold];
+    let mut buffer_index: usize = 0;
+    let input = read_input::read("inputs/06.txt");
+    for (input_index, ch) in input.chars().enumerate() {
+        if &input_index < &threshold || HashSet::<&char>::from_iter(&buffer[..]).len() < threshold {
+            buffer[buffer_index] = ch;
+            buffer_index = (&buffer_index + 1) % &threshold
+        } else {
+            return input_index;
+        }
+    }
+    panic!("Did not find marker in data");
+}
+
 fn part_1() {
-    let _input = read_input::read("inputs/06.txt");
-    println!("Part 1: {}", "Not implemented");
+    println!("Part 1: {}", find_index_of_unique_character_set(4).to_string());
 }
 
 fn part_2() {
-    println!("Part 2: {}", "Not implemented");
+    println!("Part 2: {}", find_index_of_unique_character_set(14).to_string());
 }
 
 pub fn solve() {
